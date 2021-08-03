d4fe1174223c3d66ee37a1ae09cf2a7aba2848c5
Hornsilk <hornsilk01@gmail.com>
2021-08-03 14:48:59 -0600

    basic 'gameplay': just pass constantly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import time
 
-from output_fxns import get_window_info, get_full_screen
+from waiting import wait
+
+from output_fxns import get_window_info, get_full_screen, onHomeMenu, onKeepHand
 from input_fxns import click_on, print_coord
 
 if __name__ == "__main__":
@@ -12,23 +14,12 @@ if __name__ == "__main__":
     window_info['width_fullscreen'] = 1920
     window_info['height_fullscreen'] = 1080
 
+    wait(lambda: onHomeMenu(window_info), timeout_seconds=120, waiting_for='MTGA_BOT: waiting for home menu')
+    while(onHomeMenu(window_info)):
+        click_on('PLAY_GAME')
+        time.sleep(1)
 
-
-    # print_coord()
-
-    time.sleep(1)
-    click_on('PLAY_GAME')
-    time.sleep(1)
-    click_on('PLAY_GAME')
-    time.sleep(1)
-    click_on('PLAY_GAME')
-
-    time.sleep(15)
-
-    click_on('PLAY_FIRST')
-
-    time.sleep(5)
-    
+    wait(lambda: onKeepHand(window_info), timeout_seconds=120, waiting_for='MTGA_BOT: waiting for keep hand')
     click_on('KEEP_HAND')
 
     while (True):
