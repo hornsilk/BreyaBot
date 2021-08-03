6a2ffc25d657f5f08888638fb6c4c5c48faaa239
Hornsilk <hornsilk01@gmail.com>
2021-08-03 16:31:41 -0600

    quick script to pull screenshots

---
+++ screenshot.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+
+from waiting import wait
+
+from output_fxns import get_window_info, get_full_screen, onHomeMenu, onKeepHand
+from input_fxns import click_on, print_coord
+
+if __name__ == "__main__":
+
+    window_info = get_window_info()
+
+    # make sure MTGA resolution is 1920x1080 and full screen
+    window_info['width_fullscreen'] = 1920
+    window_info['height_fullscreen'] = 1080
+
+    img = get_full_screen(window_info)
+
+    cv2.imwrite('./test.png',img)
