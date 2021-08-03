6ff5533d9adade148f4a800c875b64db8217b6a4
Hornsilk <hornsilk01@gmail.com>
2021-08-03 14:03:02 -0600

    incredibly basic: pass against sparky

--- input_fxns.py
+++ input_fxns.py
@@ -1,13 +1,32 @@
 
 from pynput import mouse, keyboard
 
+location_dict = {
+    'PLAY_GAME': (1390, 800),
+    'PLAY_FIRST': (888, 502),
+    'KEEP_HAND':(900, 695),
+    'LEFTMOST_HAND': (412, 806),
+    'DECK': (177, 773),
+    'PASS': (1422, 758),
+}
+
+
 mouse = mouse.Controller()
 kboard = keyboard.Controller()
 
-def click_on(x,y):
+def click(x,y):
     mouse.position = (x, y)
     with kboard.pressed(keyboard.Key.shift):
         kboard.press(keyboard.Key.space)
+        
+def click_on(item):
+    try:
+        (x,y) = location_dict[item]
+    except FileNotFoundError as e:
+        print('unknown item', item)
+
+    click(x,y)
+    print(f'{item}: clicked')
 
 def print_coord():
     print(mouse.position)
--- main.py
+++ main.py
@@ -2,20 +2,36 @@ import cv2
 import time
 
 from output_fxns import get_window_info, get_full_screen
-from input_fxns import click_on
+from input_fxns import click_on, print_coord
 
 if __name__ == "__main__":
-    time.sleep(2)
 
     window_info = get_window_info()
-    print(window_info)
 
     # make sure MTGA resolution is 1920x1080 and full screen
     window_info['width_fullscreen'] = 1920
     window_info['height_fullscreen'] = 1080
-    
-    full_screen = get_full_screen(window_info)
-    cv2.imwrite('./test.png', full_screen)    
 
-    print('click test')
-    click_on(window_info['width']/2, window_info['height']/2)
+
+
+    # print_coord()
+
+    time.sleep(1)
+    click_on('PLAY_GAME')
+    time.sleep(1)
+    click_on('PLAY_GAME')
+    time.sleep(1)
+    click_on('PLAY_GAME')
+
+    time.sleep(15)
+
+    click_on('PLAY_FIRST')
+
+    time.sleep(5)
+    
+    click_on('KEEP_HAND')
+
+    while (True):
+        time.sleep(1)
+        click_on('PASS')
+
