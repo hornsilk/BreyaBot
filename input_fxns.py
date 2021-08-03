3eed15f7ff51996f4ae7d1d90ff419fcb477785f
Hornsilk <hornsilk01@gmail.com>
2021-08-03 13:57:06 -0600

    fxn to print mouse coordinates

--- input_fxns.py
+++ input_fxns.py
@@ -8,3 +8,6 @@ def click_on(x,y):
     mouse.position = (x, y)
     with kboard.pressed(keyboard.Key.shift):
         kboard.press(keyboard.Key.space)
+
+def print_coord():
+    print(mouse.position)
