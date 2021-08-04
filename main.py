f76b03be38d032c05247b6a85a444c253540ce94
Hornsilk <hornsilk01@gmail.com>
2021-08-04 10:31:18 -0600

    update main game loop - played a full game w/o bugs!

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
-import cv2
 import time
-import winsound
-from waiting import wait
 
-from output_fxns import get_window_info, get_full_screen, onHomeMenu, onKeepHand, locate_leftmost_playable_card, hasPriority
-from input_fxns import click_on, print_coord, playCardAt
+from output_fxns import get_window_info, get_game_state, locate_leftmost_playable_card
+from input_fxns import click_on, playCardAt
 
 if __name__ == "__main__":
 
+    # setup
     window_info = get_window_info()
 
-    wait(lambda: onHomeMenu(window_info), timeout_seconds=120, waiting_for='MTGA_BOT: waiting for home menu')
-    while(onHomeMenu(window_info)):
-        click_on('PLAY_GAME')
-
-    wait(lambda: onKeepHand(window_info), timeout_seconds=120, waiting_for='MTGA_BOT: waiting for keep hand')
-    click_on('KEEP_HAND')
-
+    # game loop
     while (True):
-        time.sleep(1)
-        if hasPriority(window_info):
-            winsound.Beep(440,500)
+        time.sleep(0.1)
+        print('------------------------------------------------------------------')
+        state = get_game_state(window_info)
+        if state is not None:
+            print(state)
+
+        if state == 'PRIORITY':
             isPlayableCard, leftmost_card_pt = locate_leftmost_playable_card(window_info)
         
             if isPlayableCard:
                 playCardAt(window_info, leftmost_card_pt) 
             else:
                 click_on('PASS')
-        else:
-            winsound.Beep(300,500)
+                click_on('OPPONENT')
 
+        elif state == 'BLOCKING':
+            click_on('PASS')
 
+        elif state == 'ENDOFGAME':
+            click_on('CENTER')
 
-        # img = get_full_screen(window_info)
+        elif state == 'HOMESCREEN':
+            click_on('PLAY_GAME')
+
+        elif state == 'MULLIGAN':
+            click_on('KEEP_HAND')
+
+        elif state == 'LOADSCREEN':
+            click_on('CENTER')
+
+        elif state == 'INACTIVE':
+            time.sleep(0.5)
+
 
 
